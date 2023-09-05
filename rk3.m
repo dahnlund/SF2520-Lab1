8a99e52fdf86e80fe6a45063569ee6b337b135e5
dahnlund <david.ahnlund@gmail.com>
2023-09-05 11:25:40 +0200

    added rk3 for part1

---
+++ rk3.m
@@ -0,0 +1,27 @@
+% Runge-Kutta 3
+function [t, u] = rk3(dudt, T, u0, h)
+
+    t = zeros(T/h, 1);
+    u = zeros(T/h, length(u0));
+    u(1,:) = u0';
+    N = T/h;
+
+    for k = 1:N
+        tk = t(k);
+        uk = u(k,:);
+        u_new = rk3step(dudt, tk, uk', h);
+
+        u(k+1,:) = u_new;
+        t(k+1) =  k*h;
+    end
+
+
+    function u_new = rk3step(dudt, tk, uk, h)
+        k1 = dudt(tk, uk);
+        k2 = dudt(tk + h, uk + h*k1);
+        k3 = dudt(tk + h/2, uk + h*k1/4 + h*k2/4);
+        u_new = uk + h/6*(k1 + k2 + 4*k3);
+        disp(u_new)
+    end
+
+end
