bc0e7a0c9d3241e9192e893e0f604c42fb761996
dahnlund <david.ahnlund@gmail.com>
2023-09-07 10:08:12 +0200

    updated part 1a

--- part1.m
+++ part1.m
@@ -2,21 +2,13 @@
 a = 1/4 * [1, sqrt(11), 2]';
 alpha = 0.07;
 
-%-----------------------------------------------
-%Example ode to test rk3 funciton
-dudt = @(t,u) [u(2);normpdf(t,0,0.01)-0.2*u(1)];
-h = 0.01;
+%% A)
 
-[t, u_rk] = rk3(dudt, 10, [1;1], h);
+m0 = [0;0;1];
 
-[~, u_ode] = ode45(dudt, 0:h:10, [1;1]);
+dmdt = @(t,m) cross(a,m) + alpha*cross(a, cross(a,m));
+[t, m_rk] = rk3(dmdt, 50, m0, 0.001);
 
-disp(norm(u_rk(:,1)-u_ode(:,1)))
-
-plot(t, u_rk(:,1));
-hold on
-plot(t, u_ode(:,1));
-%-----------------------------------------------
 
 %% C)
 
