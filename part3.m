ad734231c62d80358985b7f6418d732e3850cbe1
dahnlund <david.ahnlund@gmail.com>
2023-09-13 17:01:44 +0200

    updated part 3

--- part3.m
+++ part3.m
@@ -8,14 +8,15 @@ r1 = 5e-2; r2 = 1.2e4; r3 = 4e7;
 
 dxdt = @(t,x) [-r1*x(1) + r2*x(2)*x(3); r1*x(1) - r2*x(2)*x(3) - r3*x(2)^2; r3*x(2)^2];
 
+T = 100;
 
-h = 7e-4:1e-6:8e-4;
+h = 3e-4:5e-5:6e-4;    % For T = 10, this is an appropriate interval to test: h = 7e-4:1e-6:8e-4
 max_vals = zeros(length(h), 1);
 for i = 1:length(h)
-    [t, u_rk] = rk3(dxdt, 10, [1;0;0], h(i));
+    [t, u_rk] = rk3(dxdt, T, [1;0;0], h(i));
 
     delta = 0.01;
-    [~, u_rk_tilde] = rk3(dxdt, 10, [1+delta;0;0], h(i));  %Introduce a small petrubation
+    [~, u_rk_tilde] = rk3(dxdt, T, [1+delta;0;0], h(i));  %Introduce a small petrubation
     
     e = abs(u_rk - u_rk_tilde)./u_rk;
     
@@ -25,9 +26,10 @@ for i = 1:length(h)
         sprintf("Max allowed h is %d", h_max)
         break
     end   
+    h_max = h(i);
 end
 
-[t, u_rk] = rk3(dxdt, 10, [1;0;0], h_max);
+[t, u_rk] = rk3(dxdt, T, [1;0;0], h_max);
 plot(e)
 
 figure
@@ -36,4 +38,18 @@ figure
 semilogy(t, u_rk)
 
 
+%% B
 
+J = @(a,b,c) [-r1, r2*c r2*b; r1, -r2*c-2*r3*b, -r2*b; 0, 2*r3*b, 0];
+
+eigs = zeros(size(u_rk));
+
+for i = 1:length(u_rk)
+    
+    eigs(i,:) = eig(J(u_rk(i,1),u_rk(i,2),u_rk(i,3)));
+
+end
+
+plot(eigs(:,1))
+figure
+plot(eigs(:,3))
