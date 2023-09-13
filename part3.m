8212ad99be38aec772c6ad0ac63e4fc449fe605d
dahnlund <david.ahnlund@gmail.com>
2023-09-13 15:57:03 +0200

    updated 3a

--- part3.m
+++ part3.m
@@ -9,20 +9,31 @@ r1 = 5e-2; r2 = 1.2e4; r3 = 4e7;
 dxdt = @(t,x) [-r1*x(1) + r2*x(2)*x(3); r1*x(1) - r2*x(2)*x(3) - r3*x(2)^2; r3*x(2)^2];
 
 
-h = 8e-4:1e-6:1e-3;
+h = 7e-4:1e-6:8e-4;
 max_vals = zeros(length(h), 1);
 for i = 1:length(h)
     [t, u_rk] = rk3(dxdt, 10, [1;0;0], h(i));
+
+    delta = 0.01;
+    [~, u_rk_tilde] = rk3(dxdt, 10, [1+delta;0;0], h(i));  %Introduce a small petrubation
     
-    max_vals(i) = max(norm(u_rk));
+    e = abs(u_rk - u_rk_tilde)./u_rk;
     
-    if isnan(max_vals(i))
-        disp("h breakpoint")
-        disp(h(i))
+    if norm(max(e)) > 10*delta   % Condition for when the solution is considered unstable
+        sprintf("Unstable for h = %d", h(i))
+        h_max = h(i-1);
+        sprintf("Max allowed h is %d", h_max)
         break
-    end
+    end   
 end
+
+[t, u_rk] = rk3(dxdt, 10, [1;0;0], h_max);
+plot(e)
+
+figure
 plot(t, u_rk)
 figure
-semilogy(t,u_rk)
+semilogy(t, u_rk)
+
+
 
